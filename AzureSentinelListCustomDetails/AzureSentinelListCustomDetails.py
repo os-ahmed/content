701b74ea8bb9a2c99b3f52e36f3df2002418347e
os-ahmed <oahmed@paloaltonetworks.com>
2024-10-13 23:53:36 +0000

    Initial Commit of AzureSentinelListCustomDetails

--- AzureSentinelListCustomDetails/AzureSentinelListCustomDetails.py
+++ AzureSentinelListCustomDetails/AzureSentinelListCustomDetails.py
@@ -7,39 +7,42 @@ import pandas as pd
 from io import BytesIO
 ''' STANDALONE FUNCTION '''
 
+
 def list_custom_details(alert_rule: dict) -> Union[dict[str, str], list]:
     """Returns a dict containing rule_id, name, sentinel_field, xsoar_type, and xsoar_field
     :type ret: ``dict``
     :return: dict as {}
     :rtype: ``dict``
     """
-    rule_id: str = alert_rule.get("name","")
-    rule_name: str = alert_rule.get("properties",{}).get("displayName", "NoName")
-    cust_details: dict = alert_rule.get("properties",{}).get("customDetails", {})
+    rule_id: str = alert_rule.get("name", "")
+    rule_name: str = alert_rule.get("properties", {}).get("displayName", "NoName")
+    cust_details: dict = alert_rule.get("properties", {}).get("customDetails", {})
     sentinel_fields = cust_details.keys()
     new_rule = [{
-                "RuleID": rule_id, "RuleName": rule_name, "XSOARType": "Empty", "XSOARField": "" ,"SentinelField": k
+                "RuleID": rule_id, "RuleName": rule_name, "XSOARType": "Empty", "XSOARField": "", "SentinelField": k
                 }
                 for k in sentinel_fields]
     ret = new_rule
 
     return ret
 
+
 def dict_to_excel_pandas(data):
     # Convert the dictionary to a DataFrame
     df = pd.DataFrame(data)
-    
+
     # Create a BytesIO object to store the Excel file
     excel_file = BytesIO()
-    
+
     # Use ExcelWriter to save the DataFrame to the BytesIO object
     with pd.ExcelWriter(excel_file, engine='xlsxwriter') as writer:
         df.to_excel(writer, index=False, sheet_name='CustomDetails')
-    
+
     # Move the cursor to the beginning of the BytesIO object
     excel_file.seek(0)
-    
+
     return excel_file.getvalue()
+
 
 def convert_dict_to_excel_and_return(alert_rules_list, filename):
     excel_data = dict_to_excel_pandas(alert_rules_list)
@@ -54,10 +57,10 @@ def list_custom_details_command(args: dict[str, Any]) -> CommandResults:
     # command_args: dict = {
     #     "limit": 200
     # }
-    
+
     # command_results: Any = execute_command(command, command_args, extract_contents=True, fail_on_error=True)
     ctxt = demisto.context()
-    command_results = ctxt.get("AzureSentinel",{}).get("AlertRule")
+    command_results = ctxt.get("AzureSentinel", {}).get("AlertRule")
     alert_rules = [list_custom_details(v) for v in command_results]
     result = [rule_pair for rule_set in alert_rules for rule_pair in rule_set]
     # return_results(convert_dict_to_excel_and_return(result,"CustomDetailsMapping.xlsx"))
@@ -70,7 +73,9 @@ def list_custom_details_command(args: dict[str, Any]) -> CommandResults:
         readable_output=json.dumps(result, indent=4)
     )
 
+
 ''' MAIN FUNCTION '''
+
 
 def main():
     try:
@@ -78,6 +83,7 @@ def main():
     except Exception as ex:
 
         return_error(f'Failed to execute AzureSentinelListCustomDetails. Error: {str(ex)}')
+
 
 ''' ENTRY POINT '''
 
